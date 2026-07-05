510e421b0d52011270167894201cd49665d9867b
Hidayath Unnisa Katlapodi <khidayathunnisa@gmail.com>
2026-07-05 10:54:13 +0530

    Connect to TiDB Cloud

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "127.0.0.1",
+    host: "mysql://4UX8xoYAQuhVWDE.root:qDUTx6glz0RMrh9g@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/github_analyzer?ssl={"rejectUnauthorized":true}"
+    ,
     user: "root",
-    password: "Hiddu", 
+    password: "Hiddu",
     database: "github_analyzer",
-    port: 3306,
+    port: 4000,
+    ssl: {
+        rejectUnauthorized: true
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
