086f33113dfc15117c0343b8c8a51a347bf4382d
Hidayath Unnisa Katlapodi <khidayathunnisa@gmail.com>
2026-07-05 11:13:37 +0530

    Fix TiDB connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "mysql://4UX8xoYAQuhVWDE.root:qDUTx6glz0RMrh9g@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/github_analyzer?ssl={"rejectUnauthorized":true}"
-    ,
+    host: "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
     user: "root",
-    password: "Hiddu",
+    password: "qDUTx6glz0RMrh9g",
     database: "github_analyzer",
     port: 4000,
     ssl: {
